aec010fa829d975efccf134e4033821f576031c1
Jackssn <mtjeck@gmail.com>
2017-01-06 17:21:21 +0400

    try to solve. logic is here

--- format_price.py
+++ format_price.py
@@ -1,5 +1,55 @@
+import argparse
+import textwrap
+
+
 def format_price(price):
-    pass
+    price = str(price).replace(' ', '')
+    if get_whole_part(price):
+        return get_whole_part(price)
+    else:
+        separators = [',', '.']
+        for separator in separators:
+            if split_validation(price.split(separator)):
+                whole_part, frac_part = price.split(separator)
+                return create_pretty_price(whole_part, frac_part)
+
+
+def create_pretty_price(integer, fraction):
+    if int(fraction) == 0:
+        return get_whole_part(integer)
+    else:
+        return "%s.%s" % (get_whole_part(integer), get_frac_part(fraction))
+
+
+def get_spaces(integer):
+    if integer:
+        integer = str(integer)[::-1]
+        return ' '.join(textwrap.wrap(integer, 3))[::-1]
+    return None
+
+
+def get_whole_part(number):
+    try:
+        return get_spaces(int(number))
+    except:
+        return None
+
+
+def get_frac_part(number):
+    try:
+        return str(round(float('0.%s' % number), 2))[2:]
+    except:
+        return None
+
+
+def split_validation(sample):
+    if len(sample) == 2:
+        return True
+    return False
+
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(description='Format price')
+    parser.add_argument('price', type=str, help='a digit for the change')
+    args = parser.parse_args()
+    print(format_price(args.price))
--- tests.py
+++ tests.py
@@ -1,1 +1,69 @@
-# TODO place your tests here
+import unittest
+
+from format_price import format_price
+
+
+class FormatPriceTestCase(unittest.TestCase):
+
+    def test_without_fractional_part(self):
+        out = format_price('100.0')
+        self.assertEqual(out, '100')
+
+    def test_with_correct_round(self):
+        out = format_price('100.039')
+        self.assertEqual(out, '100.04')
+
+    def test_with_6_digits_without_fractional_part(self):
+        out = format_price('100000')
+        self.assertEqual(out, '100 000')
+
+    def test_with_6_digits(self):
+        out = format_price('333333.55745567645')
+        self.assertEqual(out, '333 333.56')
+
+    def test_with_5_digits(self):
+        out = format_price('33333.55745567645')
+        self.assertEqual(out, '33 333.56')
+
+    def test_with_4_digits(self):
+        out = format_price('1234.01')
+        self.assertEqual(out, '1 234.01')
+
+    def test_without_fractional_part_comma(self):
+        out = format_price('100,0')
+        self.assertEqual(out, '100')
+
+    def test_with_correct_round_comma(self):
+        out = format_price('100,039')
+        self.assertEqual(out, '100.04')
+
+    def test_with_6_digits_without_fractional_part_comma(self):
+        out = format_price('100000')
+        self.assertEqual(out, '100 000')
+
+    def test_with_6_digits_comma(self):
+        out = format_price('333333,55745567645')
+        self.assertEqual(out, '333 333.56')
+
+    def test_with_5_digits_comma(self):
+        out = format_price('33333,55745567645')
+        self.assertEqual(out, '33 333.56')
+
+    def test_with_4_digits_comma(self):
+        out = format_price('1234,01')
+        self.assertEqual(out, '1 234.01')
+
+    def test_with_6_digits_without_fractional_part_space(self):
+        out = format_price('100 000 ')
+        self.assertEqual(out, '100 000')
+
+    def test_with_6_digits_space(self):
+        out = format_price('33 333 3.55745567645')
+        self.assertEqual(out, '333 333.56')
+
+    def test_with_5_digits_space(self):
+        out = format_price('33 333.55745567645')
+        self.assertEqual(out, '33 333.56')
+
+if __name__ == '__main__':
+    unittest.main()
